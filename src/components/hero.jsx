35a6bd9f9ddb9f15a9805db4c4808a1ace4e2a26
RafalDigital <rafaldigitall@gmail.com>
2026-05-03 06:56:53 +0700

    bg

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -22,7 +22,7 @@ export default function HeroSection() {
                 </div>
                 <a href="#" className='p-2 -translate-y-10 rounded-full bg-secondary border border-tertiary/20 text-tertiary active:text-primary hover:text-primary group'><RiArrowDownSLine className='group-hover:translate-y-0.5 transition-all ease-in-out' /></a>
             </div>
-            <div className="rounded-full top-5 -right-5 w-56 h-56 bg-primary opacity-50 absolute z-0"></div>
+            <div className="rounded-full top-5 -right-5 w-80 h-80 bg-primary opacity-50 absolute z-0"></div>
             <div className="rounded-full bottom-32 left-0 w-56 h-56 bg-primary opacity-50  absolute z-0"></div>
             
         </section>
