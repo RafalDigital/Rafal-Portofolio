0b7e779c3162fe287d00fe79cc0dbbe6272f7030
RafalDigital <rafaldigitall@gmail.com>
2026-05-07 20:54:35 +0700

    update bg hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -16,7 +16,7 @@ export default function HeroSection() {
 
     return (
         <section className="w-full h-dvh flex flex-col items-center relative">
-            <div className="absolute z-20 bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_90%,transparent_110%)] pointer-events-none"></div>
+            <div className="absolute z-20 bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:32px_32px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_90%,transparent_110%)] pointer-events-none"></div>
             <div className="w-full h-full flex flex-col items-center bg-transparent backdrop-blur-3xl z-10">
 
                 <div className='flex flex-col justify-center items-center w-full h-full'>
